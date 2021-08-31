90e65829e27dc11a80b4dd330e870f7f2d0f7a01
Helen WANG <wangyixuan@msn.com>
2021-08-30 23:51:12 -0700

    upload dot java file for First java Program

---
+++ FirstJavaProgram/MyOwn.java
@@ -0,0 +1,8 @@
+public class MyOwn {
+    public static void main(String[] args) {
+        System.out.println("My name is Helen Wang");
+        System.out.println("I am 400 years old");
+        System.out.println("My hometown is Shanghai");
+
+    }
+}
